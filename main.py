1b0e84d1e52be11e0b0e42a0ff6eee00d733cd49
Maia <maia.mcc@gmail.com>
2025-11-06 23:01:23 -0500

    pretend i'm an endpoint

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 def hello_world(mystery_arg):
 	now = datetime.now()
 	print(f"Hello world! - {now}\n\tmystery arg: {mystery_arg}")
+	return 'OK'  # like, it's not a web endpoint but let's pretend??
 
 if __name__ == "__main__":
-	hello_world()
+	hello_world('weh')
