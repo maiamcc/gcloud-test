a3a5e4c9caaf862b76f37b9afbdf86e40e9b3d28
Maia <maia.mcc@gmail.com>
2025-11-06 22:51:54 -0500

    update func

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 
 from datetime import datetime
 
-def hello_world():
+def hello_world(mystery_arg):
 	now = datetime.now()
-	print(f"Hello world! - {now}")
+	print(f"Hello world! - {now}\n\tmystery arg: {mystery_arg}")
 
 if __name__ == "__main__":
 	hello_world()
